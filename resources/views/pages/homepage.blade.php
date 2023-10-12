38da9fe51164888c165fe47f50ff62e7b6bb8c74
wesleywil <wesleywilsonti@gmail.com>
2023-10-12 14:31:20 -0300

    updated homepage by including the form action to add new gratitude message and msg status for validation

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -4,8 +4,12 @@
         <h2>Tip of the day</h2>
         <h3 class="w-[50rem] mx-auto text-base overflow-hidden">{{$message}}</h3>
     </div>
-    <form class="mt-2 flex flex-col gap-2">
-        <textarea type="text" placeholder="Write Here Your Gratitude" rows="10" class="px-2 py-1 text-xl font-semibold outline-0 border rounded"></textarea>
-        <button type="submit" class="w-24 px-2 py-1 text-white bg-[#4e9b69] font-semibold rounded">Submit</button>
+    <form method="POST" action="/gratitude" class="mt-2 flex flex-col gap-2">
+        @csrf
+        <textarea type="text" name="message" placeholder="Write Here Your Gratitude" rows="10" class="px-2 py-1 text-xl font-semibold outline-0 border rounded"></textarea>
+        @if (session('msg'))
+            <h1 class="text-xl text-red-400 font-bold">{{session('msg')}}</h1>
+        @endif
+        <button class="w-24 px-2 py-1 text-white bg-[#4e9b69] font-semibold rounded">Submit</button>
     </form>
 </main>
