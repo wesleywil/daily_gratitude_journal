8e5dc7cbb76288beb5bcffabaec29cbade0cbc9d
wesleywil <wesleywilsonti@gmail.com>
2023-10-16 13:06:52 -0300

    updated some of this controller functions by including the userId of the authenticated User

--- app/Http/Controllers/GratitudeController.php
+++ app/Http/Controllers/GratitudeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Gratitude;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GratitudeController extends Controller
 {
@@ -41,23 +42,22 @@ class GratitudeController extends Controller
     {
         if (empty($request->input('message'))) {
             return redirect('/')->with('msg', 'The message field is required.');
-        } else {
-            $validated = $request->validate(
-                [
-                    'message' => 'required'
-                ],
-                [
-                    'message.required' => 'The message field is required.'
-                ]
-            );
-            $gratitude = new Gratitude;
-
-            $gratitude->message = $validated['message'];
-
-            $gratitude->save();
-            return redirect('/')->with('msg', 'Message added successfully!');
-
         }
+        $validated = $request->validate(
+            [
+                'message' => 'required'
+            ],
+            [
+                'message.required' => 'The message field is required.'
+            ]
+        );
+        $gratitude = new Gratitude;
+        $gratitude->message = $validated['message'];
+        $gratitude->userId = Auth::user()->id;
+        $gratitude->save();
+        return redirect('/gratitude')->with('msg', 'Message added successfully!');
+
+
 
 
     }
@@ -66,23 +66,22 @@ class GratitudeController extends Controller
     {
         if (empty($request->input('message'))) {
             return redirect()->route('gratitude', [$id])->with('msg', 'The message field is required.');
-        } else {
-            $validated = $request->validate(
-                [
-                    'message' => 'required'
-                ],
-                [
-                    'message.required' => 'The message field is required.'
-                ]
-            );
-            $gratitude = Gratitude::find($id);
-
-            $gratitude->message = $validated['message'];
-
-            $gratitude->save();
-            return redirect('/gratitude');
-
         }
+        $validated = $request->validate(
+            [
+                'message' => 'required'
+            ],
+            [
+                'message.required' => 'The message field is required.'
+            ]
+        );
+        $gratitude = Gratitude::find($id);
+        $gratitude->message = $validated['message'];
+        $gratitude->userId = Auth::user()->id;
+        $gratitude->save();
+        return redirect('/gratitude');
+
+
     }
 
     public function delete(Request $request, string $id)
