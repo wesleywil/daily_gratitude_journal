f813073a7ef84b659b36fef37a30278b24f80790
wesleywil <wesleywilsonti@gmail.com>
2023-10-16 13:06:19 -0300

    updated Gratitude model by including the userId field with relationship to the User model

--- app/Models/Gratitude.php
+++ app/Models/Gratitude.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Gratitude extends Model
 {
     protected $table = 'journal.gratitudes';
 
-    protected $fillable = ['message'];
+    protected $fillable = ['userId','message'];
+
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class, 'userId');
+    }
 }
