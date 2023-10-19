fcb946c4de8968342e85edda1c1c7add26d83bb8
wesleywil <wesleywilsonti@gmail.com>
2023-10-19 13:25:27 -0300

    fixed homepage by swiching the buttons to archor, allowing users to navigate using the 'buttons'

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -3,11 +3,11 @@
     <h1 class="text-3xl md:text-5xl font-bold text-center">Daily Journal Gratitude</h1>
     <h2 class=" xl:w-2/4 mt-2 px-2 md:text-2xl text-center font-semibold">Embrace daily gratitude and transform your life one thankful moment at a time.</h2>
     @auth
-        <button class="w-40 mt-2 px-2 py-1 text-[#fefbfc] text-2xl bg-[#c14875] hover:bg-[#4b3735] rounded transition duration-700 ease-in-out">Journal Now</button>
+        <a href="/gratitude/create" class="w-40 mt-2 px-2 py-1 text-[#fefbfc] text-2xl bg-[#c14875] hover:bg-[#4b3735] rounded transition duration-700 ease-in-out">Journal Now</a>
         @else
-        <div class="mt-2 flex gap-2 justify-center text-[#fefbfc]">
-            <button class="w-40 px-2 py-1 text-2xl bg-[#c14875] hover:bg-[#4b3735] rounded transition duration-700 ease-in-out">Register</button>
-            <button class="w-40 px-2 py-1 text-2xl bg-[#4b3735] hover:bg-[#c14875] rounded transition duration-700 ease-in-out">Login</button>
+        <div class="mt-2 flex gap-2 justify-center text-[#fefbfc] text-center">
+            <a href="/register" class="w-40 px-2 py-1 text-2xl bg-[#c14875] hover:bg-[#4b3735] rounded transition duration-700 ease-in-out">Register</a>
+            <a href="/login" class="w-40 px-2 py-1 text-2xl bg-[#4b3735] hover:bg-[#c14875] rounded transition duration-700 ease-in-out">Login</a>
         </div>
         
     @endauth
